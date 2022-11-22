50df1f74c95d98f54981fb47b1450cb884294bba
2202Hannah <hannahbarber22@hotmail.co.uk>
2022-11-22 15:55:31 +0000

    add conditional logic to the login page

--- Components/WelcomePage.js
+++ Components/WelcomePage.js
@@ -9,10 +9,39 @@ import {
   Button,
   TouchableOpacity
 } from "react-native";
+import { fetchUsers } from "../utils";
 
 const WelcomePage = () => {
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const pressHandler = () => {
+    //setErrorMessage("");
+    //setIsLoading(true);
+    console.log(user);
+    console.log(password);
+
+    fetchUsers()
+      .then(data => {
+        console.log(data);
+        data.forEach(u => {
+          console.log(u.username === user, u.password === password);
+          if (u.username === user && u.password === password) {
+            console.log("yay!");
+            //setUser(username);
+            navigation.navigate("PositiveCollectForm", { user });
+            //setIsLoading(false);
+          }
+        });
+      })
+      .catch(() => {
+        setErrorMessage("Unable to log in, please try again");
+        setIsLoading(false);
+        setUser("");
+      });
+  };
 
   return (
     <View style={styles.container}>
@@ -24,7 +53,7 @@ const WelcomePage = () => {
           style={styles.TextInput}
           placeholder="Username."
           placeholderTextColor="#003f5c"
-          onChangeText={email => setEmail(email)}
+          onChangeText={user => setUser(user)}
         />
       </View>
 
@@ -42,7 +71,7 @@ const WelcomePage = () => {
         <Text style={styles.forgot_button}>Forgot Password?</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.loginBtn}>
+      <TouchableOpacity style={styles.loginBtn} onPress={pressHandler}>
         <Text style={styles.loginText}>LOGIN</Text>
       </TouchableOpacity>
     </View>
@@ -95,3 +124,69 @@ const styles = StyleSheet.create({
 });
 
 export default WelcomePage;
+
+// import "./styles.css";
+
+// import { useState, useContext } from "react";
+// import { fetchUsers } from "../utils";
+
+// import LoadingSpinner from "./LoadingSpinner";
+// import UserLoginContext from "../context/UserLoginContext"
+
+// const Login = () => {
+//   const {user, setUser } = useContext(UserLoginContext);
+
+//   const [username, setUsername] = useState("");
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [errorMessage, setErrorMessage] = useState("");
+//   const [successMessage, setSuccessMessage] = useState("");
+
+//   const handleSubmit = event => {
+//     event.preventDefault();
+//     setErrorMessage("");
+//     setIsLoading(true);
+//     fetchUsers()
+//       .then(data => {
+//         data.map(user => {
+//           if (user.username === username) {
+//             setUser(username);
+//             setSuccessMessage(`You are now logged in as ${username}`);
+//             setUsername("");
+//             setIsLoading(false);
+//             return user;
+//           }
+//         });
+//       })
+//       .catch(() => {
+//         setErrorMessage("Unable to log in, please try again");
+//         setIsLoading(false);
+//         setUsername("");
+//       });
+//   };
+
+//   const renderLogin = (
+//     <div className="loginForm">
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           Username:
+//           <input
+//             required
+//             name="username"
+//             value={username}
+//             onChange={event => setUsername(event.target.value)}
+//           />
+//         </label>
+//         <button type="submit">Login</button>
+//       </form>
+//       <p className="success">{successMessage}</p>
+//     </div>
+//   );
+//   return (
+//     <div className="loginPoster">
+//       {isLoading ? <LoadingSpinner /> : renderLogin}
+//       {errorMessage && <div className="error">{errorMessage}</div>}
+//     </div>
+//   );
+// };
+
+// export default Login;
